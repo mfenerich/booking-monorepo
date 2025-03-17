8a00c6f813d0bd2be110dc01cf31c9819411f527
mfenerich <marcelfenerich1@gmail.com>
2025-03-18 00:59:39 +0100

    feat(auth): add JWT and password utilities with dependencies

--- packages/libs/auth/booking_auth/__init__.py
+++ packages/libs/auth/booking_auth/__init__.py
@@ -1,1 +1,13 @@
-"""Authentication and authorization library"""
+"""Authentication and authorization library."""
+
+from .password import get_password_hash, verify_password
+from .token import create_access_token, decode_access_token, TokenSettings, TokenData
+
+__all__ = [
+    "get_password_hash",
+    "verify_password",
+    "create_access_token",
+    "decode_access_token",
+    "TokenSettings",
+    "TokenData"
+]
