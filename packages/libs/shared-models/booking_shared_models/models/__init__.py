5b511d7b22249af71663d945a4ce742eb5c8a5c4
mfenerich <marcelfenerich1@gmail.com>
2025-03-18 00:23:23 +0100

    feat(shared-models): add user schemas and models with versioning and dependencies

---
+++ packages/libs/shared-models/booking_shared_models/models/__init__.py
@@ -0,0 +1,6 @@
+# packages/shared-models/booking_shared_models/models/__init__.py
+"""Domain models package."""
+
+from .user import User, Base
+
+__all__ = ["User", "Base"]
---
+++ packages/libs/shared-models/booking_shared_models/schemas/__init__.py
@@ -0,0 +1,6 @@
+# packages/shared-models/booking_shared_models/schemas/__init__.py
+"""Schema validation models package."""
+
+from .user import User, UserBase, UserCreate, UserUpdate, UserInDB
+
+__all__ = ["User", "UserBase", "UserCreate", "UserUpdate", "UserInDB"]
