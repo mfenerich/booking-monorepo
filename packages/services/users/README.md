2cbdd4376da75b0ae2a5e0b7b213edaceb1fbf5b
mfenerich <marcelfenerich1@gmail.com>
2025-03-17 23:25:54 +0100

    feat(users): initialize user management service with structure, configuration, and tests

---
+++ packages/services/users/README.md
@@ -0,0 +1,3 @@
+# users
+
+Project description here.
---
+++ packages/services/users/tests/test_hello.py
@@ -0,0 +1,8 @@
+"""Hello unit test module."""
+
+from booking_users.hello import hello
+
+
+def test_hello():
+    """Test the hello function."""
+    assert hello() == "Hello users"
