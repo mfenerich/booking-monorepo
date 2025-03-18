f1f5119e36d75ef112189b8932dab5350b705ca8
mfenerich <marcelfenerich1@gmail.com>
2025-03-18 13:38:22 +0100

    fix: update import for declarative_base from sqlalchemy.orm

--- packages/libs/shared-models/booking_shared_models/models/user.py
+++ packages/libs/shared-models/booking_shared_models/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, DateTime, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql import func
 
 Base = declarative_base()
